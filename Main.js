06798c40cde5fe9dfa5a7799ebd37513df01b8a7
Eng Yusuf Ali Amalow <yusufaliamalow@gmail.com>
2024-07-20 13:42:42 +0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,72 @@
+const form = document.querySelector("#form-container");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirms = document.querySelector("#confirm");
+
+form.addEventListener("click", e => {
+    if (e.target.id === 'submit') {
+        e.preventDefault();
+        validateInputs();
+    }
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const confirmsValue = confirms.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 10) {
+        setError(password, 'Password must be at least 10 characters');
+    } else {
+        setSuccess(password);
+    }
+
+    if (confirmsValue === '') {
+        setError(confirms, 'Please confirm your password');
+    } else if (confirmsValue !== passwordValue) {
+        setError(confirms, 'Passwords do not match');
+    } else {
+        setSuccess(confirms);
+    }
+}
